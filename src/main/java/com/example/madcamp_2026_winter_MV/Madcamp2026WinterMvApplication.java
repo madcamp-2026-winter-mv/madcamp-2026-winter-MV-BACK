9a6eefa116e0161205041f3a1d661ccbc2492a19
seoyeon <2371013@ewhain.net>
2026-01-15 18:35:17 +0900

    [CHORE] First Commit - mv Project

---
+++ src/main/java/com/example/madcamp_2026_winter_MV/Madcamp2026WinterMvApplication.java
@@ -0,0 +1,13 @@
+package com.example.madcamp_2026_winter_MV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Madcamp2026WinterMvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Madcamp2026WinterMvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/madcamp_2026_winter_MV/Madcamp2026WinterMvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.madcamp_2026_winter_MV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Madcamp2026WinterMvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
