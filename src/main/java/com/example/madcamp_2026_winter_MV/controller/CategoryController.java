bfb7e7a6526a2d00e022043370f9c2ce707f6993
seoyeon <2371013@ewhain.net>
2026-01-18 23:46:56 +0900

    [FEAT]: 커뮤니티 추가시 탭에 자동 등록

--- src/main/java/com/example/madcamp_2026_winter_MV/controller/CategoryController.java
+++ src/main/java/com/example/madcamp_2026_winter_MV/controller/CategoryController.java
@@ -3,16 +3,12 @@ package com.example.madcamp_2026_winter_MV.controller;
 import com.example.madcamp_2026_winter_MV.dto.CategoryDto;
 import com.example.madcamp_2026_winter_MV.entity.Category;
 import com.example.madcamp_2026_winter_MV.repository.CategoryRepository;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -22,7 +18,7 @@ public class CategoryController {
 
     private final CategoryRepository categoryRepository;
 
-    // 카테고리 전체 목록 조회 API
+    // 1. 카테고리 전체 목록 조회 API
     @GetMapping
     public ResponseEntity<List<CategoryDto>> getAllCategories() {
         List<CategoryDto> categories = categoryRepository.findAll().stream()
@@ -36,5 +32,24 @@ public class CategoryController {
         return ResponseEntity.ok(categories);
     }
 
+    // 2.카테고리 생성 API (POST)
+    @PostMapping
+    public ResponseEntity<String> createCategory(@RequestBody Map<String, String> request) {
+        String name = request.get("name");
 
+        // 중복 방지 로직 (이미 있는 이름이면 생성 안 함)
+        if (categoryRepository.existsByName(name)) {
+            return ResponseEntity.badRequest().body("이미 존재하는 카테고리입니다.");
+        }
+
+        // 새 카테고리 저장
+        Category category = Category.builder()
+                .name(name)
+                .icon("✨")
+                .build();
+
+        categoryRepository.save(category);
+
+        return ResponseEntity.ok("카테고리가 생성되었습니다.");
+    }
 }
