677ab1ddecef250cc5385d113a7c65029b0cce57
seoyeon <2371013@ewhain.net>
2026-01-18 20:05:11 +0900

    [FIX]: swagger 파일 수정

--- src/main/java/com/example/madcamp_2026_winter_MV/config/SwaggerConfig.java
+++ src/main/java/com/example/madcamp_2026_winter_MV/config/SwaggerConfig.java
@@ -1,32 +1,26 @@
 package com.example.madcamp_2026_winter_MV.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        String securitySchemeName = "jwtAuth";
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securitySchemeName);
-        Components components = new Components().addSecuritySchemes(securitySchemeName, new SecurityScheme()
-                .name(securitySchemeName)
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT"));
+        Server localServer = new Server().url("http://localhost:8080").description("로컬 서버");
+        Server prodServer = new Server().url("http://madcamp-view.com").description("배포 서버");
 
         return new OpenAPI()
                 .info(new Info()
-                        .title("MadCamp MV API 명세서")
-                        .description("게시글, 좋아요, 투표, 대시보드 API 테스트용 Swagger")
+                        .title("MadCamp view API 명세서")
+                        .description("게시글, 채팅, 멤버 관리 API 명세서")
                         .version("1.0.0"))
-                .addSecurityItem(securityRequirement) // <--- addSecurityRequirements를 addSecurityItem으로 수정!
-                .components(components);
+                .servers(List.of(prodServer, localServer));
     }
 }
